25c265772a5d905ee943bf4b0a51fd1ecc78dd0d
Aravind <aravind@Aravind-MacBook-Air.local>
2024-11-06 12:28:55 +0530

    just updated

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
 export const API_URL=`https://forkify-api.herokuapp.com/api/v2/recipes/`
 export const TIME_OUT=10
 export const RES_PER_PAGE=10
+
 export const KEY='7677a00b-e4fc-478f-8956-4b8140a886f8'
